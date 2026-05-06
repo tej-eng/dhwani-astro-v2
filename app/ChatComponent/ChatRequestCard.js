a834b37ad4ee8e1c7cdcc58e8ec9994acf13f247
tejpal <tej@dhwaniastro.com>
2026-05-06 10:47:18 +0530

    change for call

--- app/ChatComponent/ChatRequestCard.js
+++ app/ChatComponent/ChatRequestCard.js
@@ -427,25 +427,27 @@ const ChatRequestCard = ({
       setTimeout(() => route.push("/chat-with-astrologer"), 100);
     };
     const handleCallAccepted = (data) => {
+      console.log("📞 CALL ACCEPTED111111111111:", data) ;
       if (data.roomId !== room_Id) return;
-
+      console.log("🚀 PROCEED TO CALL PAGE2222222222222");
       if (timerIntervalRef.current) {
+        console.log("🧹 CLEANUP TIMER BEFORE CALL333333333333") ;
         clearInterval(timerIntervalRef.current);
         timerIntervalRef.current = null;
       }
-
+      console.log("✅ TIMER CLEANED UP, PROCEEDING TO CALL44444444444") ;
       timeoutHandledRef.current = true;
-
+      console.log(" ✅ TIMEOUT HANDLED FLAG SET, PROCEEDING TO CALL555555555555") ;
       setChatActive(true);
       setShowWaitingPopup(false);
       setShowQueuePopup(false);
 
       localStorage.setItem("activeCallRoom", room_Id);
-
+      console.log("🧹 LOCALSTORAGE SET, PROCEEDING TO CALL66666666666") ;
       ["chat_request", `queue_${room_Id}`, `timer_${room_Id}`].forEach((key) =>
         localStorage.removeItem(key),
       );
-
+     console.log("🧹 LOCALSTORAGE CLEANED, PROCEEDING TO CALL777777777777") ;
       // 🚀 REDIRECT TO CALL PAGE
       route.push(`/call/${room_Id}`);
     };
